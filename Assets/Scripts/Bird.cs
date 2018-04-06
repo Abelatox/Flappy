d377e7fd12f6c453514d260ac5fbf05be9715d5a
Abelatox <abel54225@gmail.com>
2018-04-06 15:29:08 +0200

    Intent animacions

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -28,22 +28,45 @@ public class Bird : MonoBehaviour {
         if (isDead)
             return;
         
-        if (Input.GetMouseButtonDown(0))
-        {
+        if (Input.GetMouseButtonDown(0)){
             rb2d.velocity = Vector2.zero;
             rb2d.AddForce(Vector2.up * upForce);
             anim.SetTrigger("Flap");
             SoundSystem.instance.PlayFlap();
-        }        
-	}
+        }
+
+        if (Input.GetMouseButtonDown(1)){
+            rb2d.velocity = Vector2.zero;
+            rb2d.AddForce(Vector2.right * upForce);
+            anim.SetTrigger("Flap");
+            SoundSystem.instance.PlayFlap();
+        }
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        isDead = true;
-        anim.SetTrigger("Die");
-        rotateBird.enabled = false;
-        GameController.instance.BirdDie();
-        rb2d.velocity = Vector2.zero;
+        Debug.Log(collision.gameObject);
+        Column col = collision.gameObject.GetComponent<Column>();
+            if (!col.rotating){ // if not already rotating... 
+                col.rotating = true; // I'm rotating 
+                while (col.curAngle < 90)
+                {
+                    var dAngle = col.rotSpeed * Time.deltaTime; // how much to rotate this frame
+                    col.curAngle += dAngle; // updates curAngle... 
+                    this.transform.Rotate(0, dAngle, 0); // rotates object by the same angle yield;
+                                                         // suspend rotation until next frame 
+                }
+                col.curAngle -= 90;
+             // prepare for next 90 degrees rotation 
+                col.rotating = false; // I'm not rotating anymore 
+            }
+        
+        /* isDead = true;
+         anim.SetTrigger("Die");
+         rotateBird.enabled = false;
+         GameController.instance.BirdDie();
+         rb2d.velocity = Vector2.zero;*/
+        //anim.SetTrigger("Flipant");
         SoundSystem.instance.PlayHit();
     }
 }
--- Assets/Scripts/Column.cs
+++ Assets/Scripts/Column.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Column : MonoBehaviour {
+    public bool rotating = false;
+    public float curAngle;
+    public float rotSpeed = 1;
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
+       
         if (collider.CompareTag("Player"))
         {
             GameController.instance.BirdScored();
         }
+        
     }
-
 }
